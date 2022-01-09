0485992a25f7f6543862e3cf3652e7b6a1093853
shadid12 <shadidhaquee2012@gmail.com>
2022-01-09 18:55:05 -0500

    list speakers done

--- src/_client.js
+++ src/_client.js
@@ -1,5 +1,11 @@
 import { createClient } from '@urql/svelte';
 
 export default createClient({
-  url: 'http://localhost:3000/graphql',
+  url: 'https://graphql.us.fauna.com/graphql',
+  fetchOptions: () => {
+    const token = 'fnAEchxjaYAAQdooUe1q-CxPdrts3NT9BotzH15l';
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+    };
+  },
 });
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+
+<script lang="ts">
+  import { goto } from '$app/navigation';
+	import { setClient } from '@urql/svelte';
+	import { operationStore, query } from '@urql/svelte';
+	import client from '../_client'
+	setClient(client);
+  const speakers = operationStore(`
+    query {
+      speakers {
+        data {
+          _id
+          firstname
+          lastname
+          bio
+          picture
+        }
+      }
+    }
+  `)
+	query(speakers);
+
+  const navigateToProfile = (id) => {
+    goto(`/speakers/${id}`);
+  }
+</script>
+
+<h1>All Speakers</h1>
+
+{#if $speakers.fetching}
+<p>Loading...</p>
+{:else if $speakers.error}
+<p>Oh no... {$speakers.error.message}</p>
+{:else}
+<ul>
+  {#each $speakers.data.speakers.data as speaker}
+  <li>
+    <span>{speaker.firstname} {speaker.lastname}</span>
+    <button on:click={() => navigateToProfile(speaker._id)}>View</button>
+  </li>
+  {/each}
+</ul>
+{/if}
